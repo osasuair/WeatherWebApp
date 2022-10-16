a8ac271d5b22cb7956439567d5ba6b92e97bef08
Paul Airuehia <osasuair@gmail.com>
2022-10-16 14:20:52 -0400

    implemented API access throught the client.js

--- client/client.js
+++ client/client.js
@@ -4,8 +4,31 @@ function init() {
     getWeather();
 }
 
-function renderPage() {
-    
+function renderTop() {
+    let top = document.getElementById("titleDate");
+    let title = document.createElement("h1");
+    title.innerText = "Weather App";
+    top.appendChild(title);
+
+    let date = document.createElement("h2");
+    const d = new Date();
+    date.innerText = `${d.getFullYear()}/${d.getMonth()+1}/${d.getDate()}`
+    top.appendChild(date);
+
+    return d.getDay();
+}
+
+function renderDays(day) {
+    let week = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let days = document.getElementById("days");
+
+    for(let i = 0; i< 7; ++i) {
+        days.innerHTML += `<p>Day: ${week[(i+day)%7]}<br>
+                                Temperature Max: ${weather.daily.temperature_2m_max[i]} ${weather.daily_units.temperature_2m_max}<br>
+                                Temperature Min: ${weather.daily.temperature_2m_min[i]} ${weather.daily_units.temperature_2m_min}<br>
+                           <p>`;
+        days.innerHTML += "<br><br>"
+    }
 }
 
 function getWeather() {
@@ -17,6 +40,9 @@ function getWeather() {
 			//Take the response text (that is in JSON format), parse it into JS object
 			weather = JSON.parse(this.responseText);
             console.log(weather); //look at the object; what keys does it have?
+
+            let day = renderTop();
+            renderDays(day);
 		}
     }
     //request one question from a web server
